cfe97c56938f63f6617fa97912cbe296d7d1449c
fabezio <fabezio@outlook.fr>
2020-10-06 22:36:50 +0200

    clic sur rick: score incrémenté

--- index.js
+++ index.js
@@ -2,15 +2,51 @@ const scoreBoard = $(".score")
 const clouds = document.querySelectorAll(".hole")
 const heads = document.querySelectorAll(".head")
 const morty = $("#morty-play")
+let lastCloud
 let timeUp = false
 let score = 0
 // remise à zero du score
 function reset() {
-    scoreBoard.text(0)
+    score = 0
+    scoreBoard.text(score)
+}
+// morty.click(reset)
+function updateScore() {
+    score++
+    scoreBoard.text(score)
+}
+
+$(".hole1").click(updateScore)
+
+function showHead () {
+    const time = rndTime(600, 1000)
+    const cloud = rndCloud(clouds)
+    cloud.classList.add("up")
+    setTimeout(() => {
+       if(!timeUp) showHead()
+       cloud.classList.remove("up")
+
+    }, time)
+}
+function rndTime(min, max)  {
+    return Math.round(Math.random() * (max-min) + min)
+}
+function rndCloud(clouds) {
+    const thisCloudIndex = Math.floor(Math.random() * clouds.length) 
+    const thisCloud = clouds[thisCloudIndex]
+    if(thisCloud === lastCloud) return rndCloud(clouds)
+    lastCloud = thisCloud
+    return thisCloud
 
 }
 morty.click(reset)
-
 console.log(heads.length)
-scoreBoard.text(1)
+// scoreBoard.text(1)
+showHead()
 
+heads.forEach(rick => {
+    rick.addEventListener("click", function(e) {
+        if (!e.isTrusted) return
+        updateScore()
+    })
+})
